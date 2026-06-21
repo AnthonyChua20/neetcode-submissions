4d29dada25c0158291c74fa62cd044d5362daa54
AnthonyChua20 <87288431+AnthonyChua20@users.noreply.github.com>
2026-06-21 12:59:27 +0800

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0;
+        let r = 1;
+        let maxP = 0;
+       while(r<=prices.length-1){
+        if(prices[r]>prices[l]){
+            let profit = prices[r] - prices[l];
+            maxP = Math.max(maxP,profit);
+        }else{
+            l = r;
+        }
+        r++
+       }
+       return maxP;
+    }
+}
